f4a0bd43cb80480d9c1881a441d055a1dbe8e198
汪陈军 <wangcj22@chinatelecom.cn>
2025-06-29 22:20:26 +0800

    更新pom和测试

--- src/test/java/cn/wcj/demo/DemoApplicationTests.java
+++ src/test/java/cn/wcj/demo/DemoApplicationTests.java
@@ -1,13 +1,19 @@
 package cn.wcj.demo;
 
+import cn.wcj.demo.controller.TestController;
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+    @Resource
+    TestController testController;
+
     @Test
     void contextLoads() {
+        System.out.println(testController.printHelloWorld());
     }
 
 }
