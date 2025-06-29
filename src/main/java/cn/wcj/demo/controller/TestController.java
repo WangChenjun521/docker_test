df0a97e064f11866c921ee6e231a07305b31713f
汪陈军 <wangcj22@chinatelecom.cn>
2025-06-29 23:05:03 +0800

    测试本机ip

--- src/main/java/cn/wcj/demo/controller/TestController.java
+++ src/main/java/cn/wcj/demo/controller/TestController.java
@@ -4,11 +4,21 @@ package cn.wcj.demo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 @RequestMapping("/hello")
 public class TestController {
     @RequestMapping("/helloworld")
     public String printHelloWorld() {
         return "Hello World";
+    }
+
+    public void getIP() throws UnknownHostException {
+        InetAddress addr = InetAddress.getLocalHost();
+        System.out.println("Local HostAddress: " + addr.getHostAddress());
+        String hostname = addr.getHostName();
+        System.out.println("Local host name: " + hostname);
     }
 }
--- src/test/java/cn/wcj/demo/DemoApplicationTests.java
+++ src/test/java/cn/wcj/demo/DemoApplicationTests.java
@@ -5,15 +5,23 @@ import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.UnknownHostException;
+
 @SpringBootTest
 class DemoApplicationTests {
 
     @Resource
     TestController testController;
 
+
     @Test
     void contextLoads() {
         System.out.println(testController.printHelloWorld());
+    }
+
+    @Test
+    void testIp() throws UnknownHostException {
+        testController.getIP();
     }
 
 }
